c0cdb1741c4cb0e73bbdc0c3fca3a050657a1246
Josh Lee <Lee45276@gmail.com>
2015-07-02 14:43:42 +0800

    created function that detects entering and leaving projects

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+ var globals = {projects:  [],
+                oldHTML: ""};
 $(document).ready(function() {
-  var globals = {projects:  []};
+ 
 
 
   function Project(title, description, skills, url, img, github) {
@@ -13,23 +15,36 @@ $(document).ready(function() {
   };
 
   new Project("Penguin Game", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
-  new Project("Penguin Game", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
-  new Project("Penguin Game", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
-  new Project("Penguin Game", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
-  new Project("Penguin Game", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
+  new Project("Penguin Game2", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
+  new Project("Penguin Game3", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
+  new Project("Penguin Game4", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
+  new Project("Penguin Game5", "Click the penguins", "HTML, CSS, JS","Penguingame.com","./img/find_the_penguins.png","Here is is");
 
 
   function fillData (projectArray) {
     var sizes = [4, 4, 3, 3, 4];
     var i = 0;
     projectArray.forEach(function(project) {
-      $("#row_1").prepend("<li class='project col-" + sizes[i] + "'" +  "id='project-1'>Here is some text</li>");
+      $("#row_1").prepend("<li class='project col-" + sizes[i] + "'" +  "id='project-" + i + "'>Here is some text</li>");
       $("#row_1 li:first-child").css("background-image", "url(" + project["img"] + ")");
       i += 1; 
     });
   };
 
   fillData(globals.projects);
+
+
+  $(".project").hover(
+    function() {
+      // mouse over
+      globals.oldHTML = $(this).html();
+      var projectIndex = this.id.slice(-1);
+      $(this).html(globals["projects"][projectIndex]["title"]);
+    },
+    function() {
+      $(this).html(globals.oldHTML);
+  });
+
 
   //$(".project").mouseenter(function() 
   //  $(this).addClass("hidden")
